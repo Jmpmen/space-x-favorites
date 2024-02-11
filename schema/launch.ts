58a2841df5cbeaeb671eb840b0abfc9cf86d638f
Jmpmen <mendozajamesmichael@gmail.com>
2024-02-12 00:52:38 +0800

    update schema

--- schema/launch.ts
+++ schema/launch.ts
@@ -1,12 +1,30 @@
-const launchType = `
-  type Launch {
-    flight_number: Int
-    mission_name: String
-    launch_year: String
-    launch_date_utc: String
-    rocket: Rocket
-    details: String
-  }
+const launchTypes = `
+    type Launch {
+        flight_number: Int
+        mission_name: String
+        launch_year: String
+        launch_date_utc: String
+        rocket: Rocket
+        launch_site: LaunchSite
+        launch_success: Boolean
+        links: Links
+        details: String
+    }
+
+    type Rocket {
+        rocket_id: String
+        rocket_name: String
+        rocket_type: String
+    }
+
+    type LaunchSite {
+        site_name: String
+    }
+
+    type Links {
+        video_link: String
+        flickr_images: [String]
+    }
 `;
 
-export default launchType;
+export default launchTypes;
