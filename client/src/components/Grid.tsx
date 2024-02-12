36a89e60748a7eae2b8eafe952d1a0dfc6a21eb6
Jmpmen <mendozajamesmichael@gmail.com>
2024-02-12 18:46:02 +0800

    responsive grid

--- client/src/components/Grid.tsx
+++ client/src/components/Grid.tsx
@@ -2,7 +2,7 @@ import { LaunchCard } from './LaunchCard';
 
 export default function Grid({ launches }: { launches: Launch[] }) {
   return (
-    <div className="grid grid-cols-4 gap-2 mt-5">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-3 mt-5 justify-items-center">
       {launches.map((launch) => (
         <LaunchCard launch={launch} />
       ))}
